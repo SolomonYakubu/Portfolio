f1f49ac948734dcbf7ac68279358fcbcb939b8ae
Solomon-Yakubu <soloyaks.sy@gmail.com>
2020-08-02 15:04:40 +0100

    updated content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -16,19 +16,19 @@ import "react-sweet-progress/lib/style.css";
 const Skills = () => {
 	return (
 		<div>
-			<strong style={{}} className="aboutStyles">
+			<div style={{}} className="aboutP">
 				<div
 					style={{
 						borderBottomStyle: "solid",
-						borderWidth: "6px",
-						textAlign: "left",
-						width: "200px",
-						paddingTop: "20px",
+						borderWidth: "4px",
+						width: "auto",
+						padding: "none",
 					}}
+					className="aboutStyles"
 				>
 					My Skills
 				</div>
-			</strong>
+			</div>
 			<br />
 			<div className="skillContainer">
 				<div className="skills">#HTML</div> &nbsp;
@@ -59,7 +59,7 @@ const Skills = () => {
 						width: "100px",
 						paddingTop: "10px",
 						fontSize: "18px",
-						color: "#696969",
+						// color: "#000",
 					}}
 					className="aboutStyles"
 				>
@@ -82,7 +82,7 @@ const Skills = () => {
 						status="active"
 						theme={{
 							active: {
-								color: "fuchsia",
+								color: "#60935d",
 								width: "20px",
 							},
 						}}
@@ -105,7 +105,7 @@ const Skills = () => {
 						status="active"
 						theme={{
 							active: {
-								color: "fuchsia",
+								color: "#60935d",
 								width: "20px",
 							},
 						}}
@@ -128,7 +128,7 @@ const Skills = () => {
 						status="active"
 						theme={{
 							active: {
-								color: "fuchsia",
+								color: "#60935d",
 								width: "20px",
 							},
 						}}
@@ -151,7 +151,7 @@ const Skills = () => {
 						status="active"
 						theme={{
 							active: {
-								color: "fuchsia",
+								color: "#60935d",
 								width: "20px",
 							},
 						}}
@@ -174,7 +174,7 @@ const Skills = () => {
 						status="active"
 						theme={{
 							active: {
-								color: "fuchsia",
+								color: "#60935d",
 								width: "20px",
 							},
 						}}
@@ -197,7 +197,7 @@ const Skills = () => {
 						status="active"
 						theme={{
 							active: {
-								color: "fuchsia",
+								color: "#60935d",
 								width: "20px",
 							},
 						}}
@@ -220,7 +220,7 @@ const Skills = () => {
 						status="active"
 						theme={{
 							active: {
-								color: "fuchsia",
+								color: "#60935d",
 								width: "20px",
 							},
 						}}
@@ -233,11 +233,6 @@ const Skills = () => {
 };
 
 function Content(props) {
-	// const scrollers = (e) => {
-	// 	props.nameSet(e);
-
-	// 	props.scrollSet(true);
-	// };
 	if (props.scroll) {
 		scroller.scrollTo(props.name, {
 			duration: 500,
@@ -253,53 +248,31 @@ function Content(props) {
 		<div className="container" style={{}}>
 			<Options nav={props.nav} scrollers={props.scrollers} />
 			<div className="profile">
-				<div
-					className="imgCon"
-					style={
-						{
-							// width: "auto",
-							// height: "auto",
-							// padding: "0px",
-							// margin: "0px",
-						}
-					}
-				>
+				<div className="imgCon">
 					<img
 						src={require("../images/profile.jpg")}
 						style={{
 							width: "100px",
 							height: "100px",
-							borderRadius: "100px",
+							borderRadius: "170px",
 							borderStyle: "solid",
-							borderColor: "#898989",
+							borderColor: "#1f2041",
 							borderWidth: "3px",
 						}}
 					/>
 				</div>
-				{/* <div
-					style={{
-						width: "auto",
-						height: "12px",
-						padding: "10px",
-						// borderRadius: "10px",
-						// boxShadow: "0px 0px 7px 0px #696969",
-						textAlign: "center",
-						display: "flex",
-						alignItems: "center",
-						justifyContent: "center",
-					}}
-				> */}
+
 				<p style={{}} className="intro">
 					Hi!&#128075;, I'm{" "}
 					<strong
 						style={{
-							borderColor: "#a6f1a6",
-							borderWidth: "8px",
+							borderColor: "#4b3f72",
+							borderWidth: "10px",
 							borderBottomStyle: "solid",
-							color: "#a6f1a6",
+							color: "#000",
 							paddingLeft: "5px",
 							margin: "0",
-							borderLeftWidth: "0.5px",
+							borderLeftWidth: "0.9px",
 							borderLeftStyle: "solid",
 						}}
 					>
@@ -307,42 +280,42 @@ function Content(props) {
 					</strong>
 				</p>
 
-				<div
-					style={{
-						fontSize: "15px",
-						fontFamily: "Helvetica,Arial,sans serif",
-						// fontWeight: "bold",
-						color: "#696969",
-					}}
-				>
+				<div style={{}} className="typing">
 					<TypingEffect
-						text={["I'm a Full Stack web developer", "Glad to have you here"]}
-						speed={30}
-						eraseDelay={2000}
+						text={[
+							"I'm a Full-Stack web developer",
+							"I'm focused....",
+							"Creative....",
+							"Determined....",
+						]}
+						speed={15}
+						eraseDelay={1500}
 					/>
 				</div>
 
-				<p style={{}} className="profileStyles">
+				<div style={{ padding: "8px" }} className="profileStyles">
 					<strong className="personalInfo">PERSONAL INFORMATION</strong>
-					<br />
-					Birthday: 02 September
-					<br />
-					PHONE:{" "}
-					<a href="tel:+2348108405421" style={{ textDecoration: "none" }}>
-						+234 8108405421
-					</a>
-					<br />
-					MAIL:{" "}
-					<a
-						href="mailto:soloyaks.sy@gmail.com"
-						style={{ textDecoration: "none" }}
-					>
-						soloyaks.sy@gmail.com
-					</a>
-					<br />
-					Location: FCT Nigeria
-					<br />
-				</p>
+
+					<p>Birthday: 02 September</p>
+
+					<p>
+						PHONE:{" "}
+						<a href="tel:+2348108405421" style={{ textDecoration: "none" }}>
+							+234 8108405421
+						</a>
+					</p>
+
+					<p>
+						MAIL:{" "}
+						<a
+							href="mailto:soloyaks.sy@gmail.com"
+							style={{ textDecoration: "none" }}
+						>
+							soloyaks.sy@gmail.com
+						</a>
+					</p>
+					<p>Location: FCT Nigeria</p>
+				</div>
 				<div
 					style={{
 						display: "flex",
@@ -358,7 +331,7 @@ function Content(props) {
 							size="lg"
 						/>
 					</a>
-					&nbsp;
+					&nbsp; &nbsp;
 					<a href="http://www.twitter.com/@King__Solo">
 						<FontAwesomeIcon
 							icon={faTwitter}
@@ -366,7 +339,7 @@ function Content(props) {
 							size="lg"
 						/>
 					</a>
-					&nbsp;
+					&nbsp; &nbsp;
 					<a href="http://www.instagram.com/solomonyakubu34">
 						<FontAwesomeIcon
 							icon={faInstagram}
@@ -374,48 +347,50 @@ function Content(props) {
 							size="lg"
 						/>
 					</a>
-					&nbsp;
+					&nbsp; &nbsp;
 					<a href="http://www.github.com/Solomon-Yakubu">
 						<FontAwesomeIcon icon={faGithub} className="socialIcon" size="lg" />
 					</a>
-					&nbsp;
+					&nbsp; &nbsp;
 				</div>
 			</div>
 
 			<div className="contentDiv">
 				<div className="divCon" style={{}}>
-					<p style={{}} className="aboutP">
-						<strong style={{}} className="aboutStyles">
-							<div
-								style={{
-									borderBottomStyle: "solid",
-									borderWidth: "6px",
-									width: "200px",
-									padding: "none",
-								}}
-							>
-								About Me
-							</div>
-						</strong>
-						<br />I am a motivated and creative self-taught full-stack
-						developer. Currently residing at FCT, Nigeria. I am proficient in
-						vanilla Javascript and some of it's libraries and frameworks
-						including: React Js, React Native, Node.js/ExpresssJs, NextJS e.t.c.
-						Aside being a full stack developer, i am also a Computer Engineering
-						undergraduate. I offer 2 years of development experience, a strong
-						affinity for learning and meeting my goals.
-					</p>
+					<div style={{ paddingBottom: "10px" }} className="aboutP">
+						<div
+							style={{
+								borderBottomStyle: "solid",
+								borderWidth: "4px",
+								width: "auto",
+								padding: "none",
+							}}
+							className="aboutStyles"
+						>
+							About Me
+						</div>
+						<p style={{ textAlign: "center" }}>
+							I am a creative full-stack web developer and a Computer
+							Engineering undergraduate. i have a great passion for software
+							development and programming. i am proficient in Javascript and
+							it's libraries/frameworks such as ReactJs, JQuery,
+							Node.js/Expressjs etc. I am also knowledgeable with backend
+							technologies like MYSQL and MongoDB. I offer 2 years of
+							development experience. I learn fast and easily adapt to new
+							technologies. I do not believe in failure, I always work hard to
+							meet my goals.
+						</p>
+					</div>
 				</div>
-				<br />
-				<br />
+
+				<p style={{ padding: "5px" }}></p>
 
 				<div className="divCon" style={{}}>
-					<div className="mySkill" style={{ paddingBottom: "25px" }}>
+					<div className="mySkill" style={{ paddingBottom: "15px" }}>
 						<Skills />
 					</div>
 				</div>
 				<br />
-				<div className="divCon" style={{}}></div>
 			</div>
 		</div>
 	);
